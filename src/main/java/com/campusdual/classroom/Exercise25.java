da9d3a9f2c925d14323447de357bf54c043ef678
Pablo <pablobarreiro131@gmail.com>
2025-05-23 14:25:40 +0200

    Ejercicio 25. Completado y testeado.

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,15 +4,24 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
-
+        Stack<String> pila = new Stack<>();
+        pila.add("Smith");
+        pila.add("Montessori");
+        pila.add("Peralta");
+        pila.add("House");
+        return pila;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
-
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop());;
+            ;
+        }
     }
 
     public static void main(String[] args) {
-
+        Stack<String> pila = createStack();
+        printAndEmptyStack(pila);
     }
 
 
